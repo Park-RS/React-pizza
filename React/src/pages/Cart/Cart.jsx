02e6f35fffecd529c54df3c290786d99d5ff97ef
Park-RS <shwetsov27@gmail.com>
2024-06-08 19:29:39 +0700

    Реализация отправки заказа

--- React/src/pages/Cart/Cart.jsx
+++ React/src/pages/Cart/Cart.jsx
@@ -1,25 +1,55 @@
-import React from 'react';
-import minus from '../../assets/ellipse-minus.svg';
-import plus from '../../assets/ellipse-plus.svg';
-import cancel from '../../assets/ellipse-cancel.svg';
-import cart from '../../assets/cart-in-cart.svg';
-import trash from '../../assets/trash.svg';
-import path from '../../assets/path.svg';
-import pizzaTest from '../../assets/pizza_test.png';
-import { CartItem } from '../../components/CartItem';
+import React from "react";
+import cart from "../../assets/cart-in-cart.svg";
+import trash from "../../assets/trash.svg";
+import { CartItem } from "../../components/CartItem";
 
-import styles from './Cart.module.scss';
-import { useSelector, useDispatch } from 'react-redux';
-import { clearItems } from '../../redux/slices/cartSlice';
+import styles from "./Cart.module.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { clearItems } from "../../redux/slices/cartSlice";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import { TextField, DialogContent, DialogActions, Button } from "@mui/material";
 
 export const Cart = () => {
     const dispatch = useDispatch();
     const onClickClear = () => {
         dispatch(clearItems());
     };
-
     const { totalPrice, items } = useSelector((state) => state.cart);
     const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+
+    const [open, setOpen] = React.useState(false);
+    const [phone, setPhone] = React.useState('');
+    const [name, setName] = React.useState('');
+    const [address, setAddress] = React.useState('');
+
+    let order = {
+        phone: phone,
+        name: name,
+        address: address,
+        pizza: items,
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const makeOrder = () => {
+        fetch("https://fastapi-pizza-delivery.onrender.com/orders/order", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;charset=utf-8",
+            },
+            body: JSON.stringify(order),
+        }).then((res) => {
+            return res.json();
+        });
+
+        window.scrollTo(0, 0);
+        handleClose();
+    };
 
     return (
         <div className={styles.cart}>
@@ -42,7 +72,6 @@ export const Cart = () => {
                 <div className={styles.info}>
                     <div>
                         Всего пицц: <span>{totalCount} шт.</span>
-                        
                     </div>
                     <div>
                         Сумма заказа: <span>{totalPrice} ₽</span>
@@ -52,7 +81,38 @@ export const Cart = () => {
                     <button>
                         <a href="/"> Вернуться назад</a>
                     </button>
-                    <button>Оплатить сейчас</button>
+                    <button onClick={handleOpen}>Оплатить сейчас</button>
+                    <Dialog open={open} onClose={handleClose}>
+                        <DialogTitle>Форма</DialogTitle>
+                        <DialogContent style={{ width: "230px" }}>
+                            <TextField
+                                required
+                                value={phone}
+                                autoFocus
+                                margin="dense"
+                                id="outlined-error"
+                                onChange={(event) => setPhone(event.target.value)}
+                                label="Номер телефона"></TextField>
+                            <TextField
+                                required
+                                onChange={(event) => setName(event.target.value)}
+                                value={name}
+                                id="name"
+                                margin="dense"
+                                label="Имя"></TextField>
+                            <TextField
+                                required
+                                onChange={(event) => setAddress(event.target.value)}
+                                id="adress"
+                                value={address}
+                                margin="dense"
+                                label="Адрес"></TextField>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Отменить</Button>
+                            <Button onClick={makeOrder}>Сделать Заказ</Button>
+                        </DialogActions>
+                    </Dialog>
                 </div>
             </div>
         </div>
